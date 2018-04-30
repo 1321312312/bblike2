4deeb982a684d39e50256926b83174cdaa8fc9fe
too405 <38613331+toomuchm@users.noreply.github.com>
2018-04-30 16:08:32 +0800

    Update update1.sh

--- update1.sh
+++ update1.sh
@@ -4,12 +4,6 @@ echo "欢迎使用网页雷达一键脚本"
 echo "即将搭建4.27版本"
 echo "请输入你的内网ip" 
 read -p "内网ip： " ip
-cd
-cd libpcap-1.8.1
-rm -rf bblike2/
-cd 
-rm -rf bblike2/
-chmod +x restart.sh
 git clone https://github.com/1321312312/bblike2.git
 cd bblike2/
 npm i
