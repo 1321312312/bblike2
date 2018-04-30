afb441c78104380dcbdfa2ca445ca5ce478bab70
too405 <38613331+toomuchm@users.noreply.github.com>
2018-04-30 15:22:56 +0800

    Update update1.sh

--- update1.sh
+++ update1.sh
@@ -1,3 +1,4 @@
+20 lines (18 sloc)  389 Bytes
 #!/bin/bash
 echo "欢迎使用网页雷达一键脚本"
 echo "即将搭建4.27版本"
@@ -5,12 +6,12 @@ echo "请输入你的内网ip"
 read -p "内网ip： " ip
 cd
 cd libpcap-1.8.1
-rm -rf KFC2018/
+rm -rf bblike2/
 cd 
-rm -rf KFC2018/
+rm -rf bblike2/
 chmod +x restart.sh
-git clone https://github.com/1321312312/KFC2018.git
-cd KFC2018/
+git clone https://github.com/1321312312/bblike2.git
+cd bblike2/
 npm i
 npm i -g pino
 npm install -g forever
